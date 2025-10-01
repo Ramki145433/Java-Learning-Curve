b768b8716e1d3774e3c000cc6ed2115953ca102a
Rama Krishna Injarapu <injarapuramakrishna55@gmail.com>
2025-10-01 08:18:49 +0530

    learned about static(class) and non-static(instance) topics

---
+++ src/StaticAndNonStatic.java
@@ -0,0 +1,52 @@
+public class StaticAndNonStatic {
+    /*
+        | Feature    | Static (class-level)   | Non-static (instance-level)  |
+        | ---------- | ---------------------- | ---------------------------- |
+        | Belongs to | Class                  | Object                       |
+        | Memory     | Method Area            | Heap                         |
+        | Access     | ClassName.methodName() | object.methodName()          |
+        | Sharing    | Shared by all objects  | Each object has its own copy |
+        | Example    | `Math.sqrt()`          | `s1.displayInfo()`           |
+
+        Summary of execution Hierarchy
+        1.Static blocks (only once, at class load time).
+        2.Instance blocks (every time an object is created).
+        3.Constructor (every time an object is created, after instance block).
+        4.Methods (when explicitly called).
+    */
+
+    static String schoolName = "Ravindra Bharathi";
+    int rollNumber;
+    String name;
+    // non-static block
+//    {
+//        System.out.println(rollNumber + " " + name + " from non-static block");
+//    }
+    // constructor
+    StaticAndNonStatic(int num, String name){
+        this.rollNumber = num;
+        this.name = name;
+        System.out.println("From Constructor");
+    }
+    {
+        System.out.println(rollNumber + " " + name + " from non-static block");
+    }
+    //static block
+    static {
+        System.out.println(schoolName + " from static block");
+    }
+    public void display(){
+        System.out.println(name + " is studying in " + schoolName + " school");
+    }
+    public static void main(String[] args) {
+        /*
+            if you execute this code without creating object, only static blocks executes
+            because memory allocates in method area
+        */
+        // I'm creating object now
+        StaticAndNonStatic obj1 = new StaticAndNonStatic(21, "Ramki");
+        // you can in the console, 0 null from non-static block why because before executes constructor
+        //jvm checks any non static blocks are there.
+        obj1.display();
+    }
+}
